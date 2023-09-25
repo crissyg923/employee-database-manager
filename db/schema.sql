c23f0e249d2760d6a6143522c1a52a0ea3d20848
Crystal Green <crissybrown1992@icloud.com>
2023-09-25 14:16:11 -0400

    Created seeds and tested in mysql workbench

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
 CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30), 
-    SALARY INT NOT NULL,
+    SALARY DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 
@@ -23,5 +23,8 @@ CREATE TABLE employees(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id)
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+
 );
