dc444272da4bc3062c3e9f572d6a050c6230c284
Crystal Green <crissybrown1992@icloud.com>
2023-09-30 16:31:08 -0400

    Exported index file to userSelection and created asynchronous functions to fulfill db queries requested from inquirier.prompts

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const db = mysql.createConnection(
+    {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+    },
+    console.log('Connected')
+);
+
+class Database {
+    constructor(connection){
+        this.connection=connection;
+    }
+
+    viewDepartments() {
+        return this.connection.promise().query('SELECT department.id, department.department_name FROM department');
+    }
+    viewRoles() {
+        return this.connection.promise().query('SELECT department.id, department.department_name FROM department');
+    }
+
+    addEmp(x) {
+        return this.connection.promise().query('INSERT INTO employees SET ?', x);
+    }
+}
+
+module.exports = new Database(db);
+
+// db.query('SELECT * FROM roles', function(err, results) {
+//     console.log(results);
+// });
+
+// db.query('SELECT * FROM employees', function(err, results) {
+//     console.log(results);
+// });
+
+// db.query('INSERT INTO department(department_name)VALUES(?)', function(err, results) {
+
+// });
+
+// db.query('INSERT INTO roles(title, salary, department_id)', function(err, results) {
+
+// });
+
+// db.query('INSERT INTO employees(first_name, last_name, role_id, manager_id', function(err, results) {
+
+// });
+
+// db.query('Update employees SET ? Where id=?', function(err, results) {
+
+// });
--- userSelection.js
+++ userSelection.js
@@ -1,24 +1,146 @@
 const inquirer=require('inquirer');
+const db = require('./index');
+// require('console.table');
 
-inquirer.prompt([
-    {
-        type: 'list',
-        name: 'selection',
-        choices:[
-            'View All Departments',
-            'View All Roles',
-            'View All Employees',
-            'Add a Department',
-            'Add a Role',
-            'Add an Employee',
-            'Update an Employee Role'
-        ]
-    }
-])
-
-.then(({selection}) => {
-    if(selection==='View All Departments') {
-        console.log('Viewing All Departments');
-    }
+function init() {
+    
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'selection',
+            choices:[
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update an Employee Role',
+                'exit'
+            ]
+        }
+    ])
+    
+    .then(({selection}) => {
+        if(selection==='View All Departments') {
+            console.log('Viewing All Departments');
+            viewDepartments();
+        }
+        else if(selection==='View All Roles') {
+            console.log('Viewing All Roles');
+            viewRoles();
+        }
+        else if(selection==='View All Employees') {
+            console.log('Viewing All Employes');
+        }
+        else if(selection==='Add a Department') {
+            console.log('Adding a department');
+            addDepartment();
+        }
+        else if(selection==='Add a Role') {
+            console.log('Adding a Role');
+            addRole();
+        }
+        else if(selection==='Add an Employee') {
+            console.log('Adding an Employee');
+            addEmployee();
+        }
+        else if(selection==='Updating Employee') {
+            console.log('Adding an Employee');
+            // addEmployee(); 
+        }
+        else{
+            console.log('Updating Employee');
+            end();
+        }
+    });
 }
-)
+
+
+function viewDepartments() {
+    db.viewDepartments()
+    .then(([response]) => {
+        console.table(response);
+        init();
+    })
+}
+
+function end() {
+    return process.exit()
+}
+
+function addDepartment () {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'department',
+        message: 'Please enter the name of the department.'
+    }])
+    .then(({department}) => {
+        console.log(department);
+    })
+};
+
+function addRole () {
+    return inquirer.prompt([
+        {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of the role.'
+         },
+         {
+            type: 'input',
+            name: 'salary',
+            message: 'Please enter the salary for this role.'
+         },
+         {
+         type: 'input',
+         name: 'departmentId',
+         message: 'Please enter the department ID associated with this role.'
+         }
+])
+    .then(({title, salary, departmentId}) => {
+        console.log(title, salary, departmentId);
+    })
+};
+
+function addEmployee () {
+    return inquirer.prompt([
+        {
+        type: 'input',
+        name: 'firstname',
+        message: 'Please enter the first name of the employee.'
+         },
+         {
+         type: 'input',
+         name: 'lastname',
+         message: 'Please enter the last name of the employee.'
+         },
+         {
+         type: 'input',
+         name: 'roleId',
+         message: 'Please enter the role ID associated with the role of this employee.'
+         },
+         {
+         type: 'input',
+         name: 'managerId',
+         message: 'Please enter the manager ID asocisated with this employee.'
+         },
+
+])
+    // .then(({firstname, lastname, roleId, managerId}) => {
+    .then((data) => {
+        let emp = {
+            first_name: data.firstname,
+            last_name: data.lastname,
+            role_id: data.roleId,
+            manager_id: data.managerId
+        }
+        // console.log(firstname, lastname, roleId, managerId);
+        db.addEmp(emp)
+        // call view employee
+        console.log("Employee added!");
+    })
+};
+
+init()
+
